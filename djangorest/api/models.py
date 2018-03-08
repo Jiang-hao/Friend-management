5a9d0a1c27fd3c2472ca560f6bd37485075c0a03
jiang-hao <jiangh0313@gmail.com>
2018-03-09 01:10:08 +0800

    Finished 5th api, but did not add validations for blocked friends in connect ohyeah

--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -2,20 +2,21 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class Person(models.Model):
-	name = models.CharField(max_length=50, unique=True, blank=False, primary_key=True)
-
+	name = models.CharField(max_length=200, unique=True, blank=False, primary_key=True)
+	post_message = models.CharField(max_length = 200, default='') # self post
+	received_message = models.CharField(max_length = 200, default='') # receive from others
 
 class Friends(models.Model):
 	person = models.ForeignKey(Person,on_delete=models.CASCADE)
 	friends = ArrayField(
-		models.CharField(max_length=50,null=True)
+		models.CharField(max_length=200,null=True)
 		)
 	blocked_friends = ArrayField(
-		models.CharField(max_length=50,null=True)
+		models.CharField(max_length=200,null=True)
 		)
-	follows = ArrayField(
-		models.CharField(max_length=50,null=True)
+	follower = ArrayField(
+		models.CharField(max_length=200,null=True)
 		) # Friends subscribe this person
-	following = ArrayField(
-		models.CharField(max_length=50,null=True)
+	follow = ArrayField(
+		models.CharField(max_length=200,null=True)
 		) # Friends this person subscribes
--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -41,44 +41,56 @@ def listAll(request):
 	persons = Person.objects.all()
 	context = {'allPersons': persons}
 	return render(request,'templates/index.html',context)
-	# for a in persons:
-	# 	friend_list = Friends.objects.filter(person = a)
-	# 	if(bool(friend_list)):
-	# 		# True if not empty
-	# 		res[a.name] = friend_list.get().friends
-	# 	else:
-	# 		res[a.name] = ['No friends']
-
-	# res['1'] = 1
-	# res['2'] = 3
-	#data = serializers.serialize('json', res)
-	#return HttpResponse(json.dumps(res))
 
 
 @csrf_exempt
 def connect(request):
-
- 	requestor = request.POST.get('requestor')
- 	target = request.POST.get('target')
- 	res={}
- 	try:
+	data = request.POST.getlist('friends[]')
+	requestor = data[0]
+	target = data[1]
+	res={}
+	try:
 	 	if bool(Person.objects.filter(name=requestor)):
 	 		#Record exists
 	 		requestor_obj = Person.objects.get(name=requestor)
 	 	else:
 	 		#No records
-	 		Person.objects.create(name=requestor)
+	 		Person.objects.create(name=requestor,post_message='',receive_message='')
 	 		requestor_obj = Person.objects.get(name=requestor)
-	 	
-	 	if bool(Friends.objects.filter(person=requestor_obj)):
+	 		Friends.objects.create(person=requestor_obj,friends=[], blocked_friends=[],follower=[],follow=[] )
+
+
+	 	if bool(Person.objects.filter(name=target)):
 	 		#Record exists
-	 		requestor_friends = Friends.objects.get(person=requestor_obj)
-	 		requestor_friends.friends.append(target)
-	 		requestor_friends.save()
+	 		target_obj = Person.objects.get(name=target)
 	 	else:
 	 		#No records
-	 		Friends.objects.create(person=requestor_obj,friends=[target], blocked_friends=[],follows=[],following=[] )	
- 		res['success'] = True
+	 		Person.objects.create(name=target,post_message='',receive_message='')
+	 		target_obj = Person.objects.get(name=target)
+	 		Friends.objects.create(person=target_obj,friends=[], blocked_friends=[],follower=[],follow=[] )
+	 	
+	 	try:
+	 		#Record exists
+	 		requestor_friends = Friends.objects.get(person=requestor_obj)
+	 		if target not in requestor_friends.friends:
+		 		requestor_friends.friends.append(target)
+		 		requestor_friends.save()
+		 		res['success'] = True
+		 	else:
+		 		res['success'] = False
+		 		res['reason'] = target + " is already a friend of "+requestor
+	 		
+	 		target_friends = Friends.objects.get(person=target_obj)
+	 		if requestor not in target_friends.friends:
+		 		target_friends.friends.append(requestor)
+		 		target_friends.save()
+		 		res['success'] = True
+		 	else:
+		 		res['success'] = False
+		 		res['reason'] = target + " is already a friend of "+requestor
+	 	except Exception as e:
+	 		res['success'] = False
+	 		res['reason'] = "Error occured: "+ str(e) 
  	# try:
  	# 	requestor_friends = Friends.objects.get(person=requestor_obj)
  	# 	requestor_friends.friends.append(target)
@@ -86,6 +98,170 @@ def connect(request):
  	# except:
  	# 	res['success'] = False
  	# res['success'] = requestor_obj.name
- 	except:
- 		res['success'] = False		
- 	return HttpResponse(json.dumps(res))
+	except Exception as e:
+ 		res['success'] = False
+ 		res['reason'] = "Error occured: "+ str(e)	
+	return HttpResponse(json.dumps(res))
+
+@csrf_exempt
+def retrieve(request):
+	res={}
+	email = request.POST.get('email')
+	try:
+		the_person = Person.objects.get(name=email)
+
+	except:
+		res['success'] = False
+		res['reason'] = email + " is not in Person database"
+		return HttpResponse(json.dumps(res))
+	try:
+		friend_list = Friends.objects.get(person=the_person).friends
+	except:
+		res['success'] = False
+		res['reason'] = email + " is not in Friends database"
+		return HttpResponse(json.dumps(res))
+
+	
+	res['success'] = True
+	res['friends'] = friend_list
+	res['count'] = len(friend_list)
+
+	return HttpResponse(json.dumps(res))
+
+
+@csrf_exempt
+def common(request):
+	data = request.POST.getlist('friends[]')
+	requestor = data[0]
+	target = data[1]
+	res={}
+	try:
+		requestor_person = Person.objects.get(name=requestor)
+
+	except:
+		res['success'] = False
+		res['reason'] = requestor + " is not in Person database"
+		return HttpResponse(json.dumps(res))
+	try:
+		target_person = Person.objects.get(name=target)
+
+	except:
+		res['success'] = False
+		res['reason'] = target + " is not in Person database"
+		return HttpResponse(json.dumps(res))
+
+	try:
+		requestor_friend_list = Friends.objects.get(person=requestor_person).friends
+	except:
+		res['success'] = False
+		res['reason'] = requestor+" is not in Friends database"
+		return HttpResponse(json.dumps(res))
+	try:
+		target_friend_list = Friends.objects.get(person=target_person).friends
+	except:
+		res['success'] = False
+		res['reason'] = target+" is not in Friends database"
+		return HttpResponse(json.dumps(res))
+	
+	commont_friends= list(set(requestor_friend_list).intersection(target_friend_list))	
+
+
+	res['success'] = True
+	res['friends'] = commont_friends
+	res['count'] = len(commont_friends)
+
+	return HttpResponse(json.dumps(res))
+
+@csrf_exempt
+def follow(request):
+	
+	res={}
+	requestor = request.POST.get('requestor')
+	target = request.POST.get('target')
+	try:
+		requestor_person = Person.objects.get(name=requestor)
+
+	except:
+		#Not in Person db. Create one for requestor
+		Person.objects.create(name=requestor,post_message='',receive_message='')
+		requestor_person = Person.objects.get(name=requestor)
+		Friends.objects.create(person=requestor_person,friends=[],blocked_friends=[],follower=[],follow=[])
+	
+
+	try:
+		target_person = Person.objects.get(name=target)
+
+	except:
+		#Not in Person db. Create one for requestor
+		Person.objects.create(name=target,post_message='',receive_message='')
+		target_person = Person.objects.get(name=target)
+		Friends.objects.create(person=target_person,friends=[],blocked_friends=[],follower=[],follow=[])
+
+	
+	try:
+		requestor_follow = Friends.objects.get(person=requestor_person)
+		requestor_follow.follow.append(target)
+		requestor_follow.save()
+
+	except:
+		res['success'] = False
+		res['reason'] = requestor + " cannot follow "+ target
+		return HttpResponse(json.dumps(res))
+	
+
+	try:
+		target_follower = Friends.objects.get(person=target_person)
+		target_follower.follower.append(requestor)
+		target_follower.save()
+	except:
+		res['success'] = False
+		res['reason'] = target + " cannot add " +requestor+" as follower"
+		return HttpResponse(json.dumps(res))
+
+
+	
+	res['success'] = True
+
+	return HttpResponse(json.dumps(res))
+
+
+
+@csrf_exempt
+def block(request):
+	
+	res={}
+	requestor = request.POST.get('requestor')
+	target = request.POST.get('target')
+	try:
+		requestor_person = Person.objects.get(name=requestor)
+
+	except:
+		#Not in Person db. Create one for requestor
+		Person.objects.create(name=requestor,post_message='',receive_message='')
+		requestor_person = Person.objects.get(name=requestor)
+		Friends.objects.create(person=requestor_person,friends=[],blocked_friends=[],follower=[],follow=[])
+	
+
+	try:
+		target_person = Person.objects.get(name=target)
+
+	except:
+		#Not in Person db. Create one for requestor
+		Person.objects.create(name=target,post_message='',receive_message='')
+		target_person = Person.objects.get(name=target)
+		Friends.objects.create(person=target_person,friends=[],blocked_friends=[],follower=[],follow=[])
+
+	
+	try:
+		requestor_blocks = Friends.objects.get(person=requestor_person)
+		requestor_blocks.blocked_friends.append(target)
+		requestor_blocks.save()
+
+	except:
+		res['success'] = False
+		res['reason'] = requestor + " cannot block "+ target
+		return HttpResponse(json.dumps(res))
+	
+	res['success'] = True
+
+	return HttpResponse(json.dumps(res))
